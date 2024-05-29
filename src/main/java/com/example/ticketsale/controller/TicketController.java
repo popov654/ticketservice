cc30ca304bde95f3ee0705b4964268b62e5338a1
popov654 <popov654@yandex.ru>
2024-05-29 06:21:25 +0300

    Refactoring paths

--- src/main/java/com/example/ticketsale/controller/TicketController.java
+++ src/main/java/com/example/ticketsale/controller/TicketController.java
@@ -8,32 +8,30 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/tickets")
 @AllArgsConstructor
 public class TicketController {
 
     private final TicketService ticketService;
 
-    @GetMapping("/ticket/{id}")
+    @GetMapping("/show/{id}")
     public Ticket get(@PathVariable("id") UUID id) {
         return ticketService.getTicketById(id);
     }
 
-    @GetMapping("/ticket/{event_id}/new")
+    @GetMapping("/event/{event_id}/new")
     public Ticket create(@PathVariable("event_id") long eventId, @RequestParam("cost") @DefaultValue("500") String cost) {
         return ticketService.createTicket(eventId, new BigDecimal(cost));
     }
 
-    @GetMapping("/tickets/event/{event_id}")
+    @GetMapping("/event/{event_id}")
     public List<Ticket> getByEvent(@PathVariable("event_id") long eventId) {
         return ticketService.getTicketByEventId(eventId);
     }
